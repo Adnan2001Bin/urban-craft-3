79243e0059829d13a96a33934ecdcdab611ed666
Adnan Bin Hossen <hossen15-4543@diu.edu.bd>
2024-10-19 18:38:46 +0600

    configure redux toolkit

---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import Header from "./Header/Header";
+import Footer from "./footer/Footer";
+// import LogoutBtn from "./Header/LogoutBtn";
+// import Button from "./Button";
+// import Input from "./Input";
+// import Login from "./Login";
+// import Signup from "./Signup";
+// import PostForm from "./post-form/PostForm";
+// import Select from "./Select"
+// import AuthLayout from "./AuthLayout"
+// import PostCard from "./PostCard";
+export {
+    Header,
+    Footer,
+    // LogoutBtn,
+    // Button,
+    // Input,
+    // Login,
+    // Signup,
+    // PostForm,
+    // Select,
+    // AuthLayout,
+    // PostCard
+
+}
