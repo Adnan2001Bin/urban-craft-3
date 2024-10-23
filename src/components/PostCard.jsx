27f861dd01182504d189addd43697c76e4a694f3
Adnan Bin Hossen <hossen15-4543@diu.edu.bd>
2024-10-23 17:47:50 +0600

    PostCard component add

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import appwriteService from "../appwrite/config";
+import parse from "html-react-parser";
+
+import { Link } from "react-router-dom";
+
+function PostCard({ $id, homePageImg1, homePageImg2, homePageImg3 }) {
+  return (
+    <Link to={`/post/${$id}`}>
+      <div className="bg-[#f5f5d6] p-4 rounded-lg shadow-md w-80">
+        <div className="bg-white rounded-lg overflow-hidden">
+          <img
+            src={appwriteService.getFilePreview(homePageImg1)}
+            alt="homePageImg1"
+            className="w-full h-48 object-cover"
+          />
+
+          <img
+            src={appwriteService.getFilePreview(homePageImg2)}
+            alt="homePageImg1"
+            className="w-full h-48 object-cover"
+          />
+
+          <img
+            src={appwriteService.getFilePreview(homePageImg3)}
+            alt="homePageImg1"
+            className="w-full h-48 object-cover"
+          />
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+export default PostCard;
