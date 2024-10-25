0ea3a286c4e1128c6d925f2820dbdd941ee68f12
Adnan Bin Hossen <hossen15-4543@diu.edu.bd>
2024-10-25 18:06:19 +0600

    make some change

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,94 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import { AuthLayout, Login, Signup } from "./components/index.js";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+import AddHomepageImg from './pages/AddHomepageImg.jsx'
+import EditPost from "./pages/EditPost.jsx";
+import Post from "./pages/Post.jsx";
+import AllPosts from "./pages/AllPosts.jsx";
+// import BedRoomCatagoriesPage from "./components/ProductCatagories/Beedroom/BedRoomCatagoriesPage.jsx"
+// import DinningRoomCatagoriesPage from "./components/ProductCatagories/Dinning/DinningRoomCatagoriesPage.jsx"
+// import Signup from './pages/AddPost.jsx'
+// import EditPost from "./pages/EditPost.jsx";
+
+// import Post from "./pages/Post.jsx";
+
+// import AllPosts from "./pages/AllPosts.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: (
+          <AuthLayout authentication={false}>
+            <Login />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/signup",
+        element: (
+          <AuthLayout authentication={false}>
+            <Signup />
+          </AuthLayout>
+        ),
+      },
+
+      {
+        path: "/AddHomePageImg",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <AddHomepageImg />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/EditHomePageImg/:slug",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <EditPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/post/:slug",
+        element: <Post />,
+      },
+      {
+        path: "/All HomePage Img",
+        element: <AllPosts />,
+      },
+      // {
+      //   path: "/BedRoomCatagoriesPage",
+      //   element: <BedRoomCatagoriesPage />,
+      // },
+      // {
+      //   path: "/DinningRoomCatagoriesPage",
+      //   element: <DinningRoomCatagoriesPage />,
+      // },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
-  </StrictMode>
+  </React.StrictMode>
 );
---
+++ src/pages/Post.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import appwriteService from "../appwrite/config";
+import { Button } from "../components";
+import parse from "html-react-parser";
+import { useSelector } from "react-redux";
+
+export default function Post() {
+  const [post, setPost] = useState(null);
+  const { slug } = useParams();
+  const navigate = useNavigate();
+
+  const userData = useSelector((state) => state.auth.userData);
+
+  const isAuthor = post && userData ? post.userId === userData.$id : false;
+
+  useEffect(() => {
+    if (slug) {
+      appwriteService.getPost(slug).then((post) => {
+        if (post) setPost(post);
+        else navigate("/");
+      });
+    } else navigate("/");
+  }, [slug, navigate]);
+
+  const deletePost = async () => {
+    if (post) {
+      const status = await appwriteService.deletePost(post.$id);
+      if (status) {
+        // Delete all associated images
+        if (post.homePageImg1) {
+          await appwriteService.deleteFile(post.homePageImg1);
+        }
+        if (post.homePageImg2) {
+          await appwriteService.deleteFile(post.homePageImg2);
+        }
+        if (post.homePageImg3) {
+          await appwriteService.deleteFile(post.homePageImg3);
+        }
+        navigate("/all-menu");
+      }
+    }
+  };
+
+  return post ? (
+    <div className="w-full flex justify-center bg-white my-4">
+      <div className="w-96 shadow-2xl bg-gray-50 text-stone-900 p-2">
+        <figure>
+          {post.homePageImg1 && (
+            <img
+              src={appwriteService.getFilePreview(post.homePageImg1)}
+              alt="Home Page Image 1"
+              className="rounded-xl mb-4"
+            />
+          )}
+          {post.homePageImg2 && (
+            <img
+              src={appwriteService.getFilePreview(post.homePageImg2)}
+              alt="Home Page Image 2"
+              className="rounded-xl mb-4"
+            />
+          )}
+          {post.homePageImg3 && (
+            <img
+              src={appwriteService.getFilePreview(post.homePageImg3)}
+              alt="Home Page Image 3"
+              className="rounded-xl mb-4"
+            />
+          )}
+        </figure>
+        <div className="card-body">
+          <h1 className="font-fontFooter1 text-xl font-medium mb-1 mt-1">
+            {post.title}
+          </h1>
+          
+          {isAuthor && (
+            <div className="flex justify-end space-x-4">
+              <Link to={`/EditHomePageImg/${post.$id}`}>
+                <Button className="bg-green-500 w-20">Edit</Button>
+              </Link>
+              <Button className="bg-red-700 w-20" onClick={deletePost}>
+                Delete
+              </Button>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  ) : null;
+}
