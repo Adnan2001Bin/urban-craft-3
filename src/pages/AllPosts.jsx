931fcece04e3f89d9e01f1d493d0666f113b0eb4
Adnan Bin Hossen <hossen15-4543@diu.edu.bd>
2024-10-25 23:20:57 +0600

    remove map

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -1,33 +1,34 @@
 import React, { useState, useEffect } from "react";
-import {  PostCard } from "../components";
+import { PostCard } from "../components";
 import appwriteService from "../appwrite/config";
 
 function AllPosts() {
-  const [posts, setPosts] = useState([]);
-  useEffect(() => {}, []);
-  appwriteService.getPosts([]).then((posts) => {
-    if (posts) {
-      setPosts(posts.documents);
-    }
-  });
+  const [post, setPost] = useState(null); // Change to store a single post
 
-  if (posts.length === 0) {
+  useEffect(() => {
+    appwriteService.getPosts([]).then((response) => {
+      if (response && response.documents.length > 0) {
+        setPost(response.documents[0]); // Set only the first post
+      }
+    });
+  }, []);
+
+  if (!post) {
     return (
       <div className="w-full h-96 flex justify-center items-center font-fontFooter1 text-black text-5xl">
         <h1>Posts is empty</h1>
       </div>
     );
   }
+
   return (
     <div className="w-full py-8 px-20">
-        <div className="w-full flex justify-center font-fontFooter font-bold text-5xl"><h1>Our Menu</h1></div>
-        <div className="grid grid-cols-4">
-          {posts.map((post) => (
-            <div key={post.$id} className="p-2 w-1/4">
-              <PostCard {...post} />
-            </div>
-          ))}
-        </div>
+      <div className="w-full flex justify-center font-fontFooter font-bold text-5xl">
+        <h1>Home Page Image</h1>
+      </div>
+      <div className="flex justify-center p-2 w-full">
+        <PostCard {...post} />
+      </div>
     </div>
   );
 }
